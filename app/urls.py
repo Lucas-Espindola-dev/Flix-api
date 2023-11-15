c390d4a7e9ac4edd0b191245a48c9e52a910507f
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-15 13:53:03 -0300

    first commit

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from genres.views import genre_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('genres/', genre_view, name='genre-list')
+]
---
+++ genres/views.py
@@ -0,0 +1,8 @@
+from django.http import JsonResponse
+from genres.models import Genre
+
+
+def genre_view(request):
+    genres = Genre.objects.all()
+    data = [{'id': genre.id, 'name': genre.name} for genre in genres]
+    return JsonResponse(data, safe=False)
