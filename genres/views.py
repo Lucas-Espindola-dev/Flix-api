97284bfa03132b4c5d9547c15251123c962dd888
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 09:02:33 -0300

    Creating the CreateList Class

--- genres/views.py
+++ genres/views.py
@@ -1,8 +1,8 @@
 from django.http import JsonResponse
 from genres.models import Genre
+from rest_framework import generics
 
 
-def genre_view(request):
-    genres = Genre.objects.all()
-    data = [{'id': genre.id, 'name': genre.name} for genre in genres]
-    return JsonResponse(data, safe=False)
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = None
