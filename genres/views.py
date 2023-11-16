2bcdb34057f98e7a997290ff992abf5e74f4a8ba
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 10:23:25 -0300

    Creating new class

--- genres/views.py
+++ genres/views.py
@@ -1,4 +1,3 @@
-from django.http import JsonResponse
 from genres.models import Genre
 from rest_framework import generics
 from genres.serializers import GenreSerializer
@@ -8,3 +7,7 @@ class GenreCreateListView(generics.ListCreateAPIView):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
+
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
