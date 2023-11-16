09df3bcd586e2f9a3a9cf120ebb348cd0f72c1d7
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 14:11:31 -0300

    Creating Views and serializers.py

--- actors/views.py
+++ actors/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from actors.models import Actor
+from rest_framework import generics
+from genres.serializers import GenreSerializer
 
-# Create your views here.
+
+class GenreCreateListView(generics.ListCreateAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = GenreSerializer
+
+
+class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Actor.objects.all()
+    serializer_class = GenreSerializer
+
