83c9e5ec3e3f410bfcb420a40e714262430b2d41
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 10:40:42 -0300

    Adding the new urls

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import GenreCreateListView
+from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', GenreCreateListView.as_view(), name='genre-list')
+    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
 ]
