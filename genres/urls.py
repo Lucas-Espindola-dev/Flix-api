967c465ffbd3e3bb1ed9f85dcda6f04a541d3c44
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 14:29:53 -0300

    Refatoring urls

---
+++ genres/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from genres.views import GenreCreateListView, GenreRetrieveUpdateDestroyView
+
+
+urlpatterns = [
+    path('genres/', GenreCreateListView.as_view(), name='genre-create-list'),
+    path('genres/<int:pk>/', GenreRetrieveUpdateDestroyView.as_view(), name='genre-detail-view'),
+]
