33ce4f481b4da11a03867791e4a1ba79bb1b0fe6
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 13:58:14 -0300

    Function str

--- actors/models.py
+++ actors/models.py
@@ -11,5 +11,8 @@ NATIONALITY_CHOICES = (
 class Actor(models.Model):
     name = models.CharField(max_length=200)
     birthday = models.DateField(blank=True, null=True)
-    nationality = models.CharField(max_length=100, choices=NATIONALITY_CHOICES, )
+    nationality = models.CharField(max_length=100, choices=NATIONALITY_CHOICES, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
 
