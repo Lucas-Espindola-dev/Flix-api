f9d005e4e9ba3baeaff69c0c128f95e9d87dd61d
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 13:51:11 -0300

    Creating Model for Actor

--- actors/models.py
+++ actors/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 
+class Actor(models.Model):
+    name = models.CharField(max_length=200)
+    birthday = models.DateField(blank=True, null=True)
+    nationality = models.CharField(max_length=100)
+
