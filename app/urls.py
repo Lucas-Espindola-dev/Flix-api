64d4925624d9958555e5bd2edef4aff0d2ae8769
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 09:21:24 -0300

    Importing the new class as view

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path
-from genres.views import genre_view
+from genres.views import GenreCreateListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_view, name='genre-list')
+    path('genres/', GenreCreateListView.as_view(), name='genre-list')
 ]
