63915572579b83b4c8bba7376b51486ee237c30e
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 14:47:35 -0300

    Correcting Serializer

--- actors/serializers.py
+++ actors/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from actors.models import Actor
 
 
-class ActorSerializer(serializers.Serializer):
+class ActorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Actor
         fields = '__all__'
