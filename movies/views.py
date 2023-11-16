52e06088c047485248500415d367de68e7988737
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 15:59:47 -0300

    Importing Movies Serializers

--- movies/views.py
+++ movies/views.py
@@ -1,12 +1,13 @@
 from rest_framework import generics
 from movies.models import Movie
+from movies.serializers import MovieSerializers
 
 
 class MoviesCreateListView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
-    serializer_class = None
+    serializer_class = MovieSerializers
 
 
 class MoviesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
-    serializer_class = None
+    serializer_class = MovieSerializers
