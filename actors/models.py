2c1a14ffe1399940439d8ac8acd535a7d0ce7e7f
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 13:55:41 -0300

    Creating nacionality choices

--- actors/models.py
+++ actors/models.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 
+NATIONALITY_CHOICES = (
+    ('USA', 'United States'),
+    ('BRA', 'Brazil'),
+    ('CUB', 'Cuba')
+)
+
+
 class Actor(models.Model):
     name = models.CharField(max_length=200)
     birthday = models.DateField(blank=True, null=True)
-    nationality = models.CharField(max_length=100)
+    nationality = models.CharField(max_length=100, choices=NATIONALITY_CHOICES, )
 
