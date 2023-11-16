6343365aa69c04cdea267582b5ba3a5f3ecc8944
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 15:44:14 -0300

    Creating the Movie Model

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from genres.models import Genre
+from actors.models import Actor
 
-# Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=500)
+    genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name='movies')
+    release_date = models.DateField(null=True, blank=True)
+    cast = models.ManyToManyField(Actor, related_name='movies')
+    resume = models.TextField
