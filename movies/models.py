e3f7c02f6d2890ebe9ee9aee2e1b1be25835ac82
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-16 15:46:05 -0300

    Adding the str function

--- movies/models.py
+++ movies/models.py
@@ -8,4 +8,7 @@ class Movie(models.Model):
     genre = models.ForeignKey(Genre, on_delete=models.PROTECT, related_name='movies')
     release_date = models.DateField(null=True, blank=True)
     cast = models.ManyToManyField(Actor, related_name='movies')
-    resume = models.TextField
+    resume = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
