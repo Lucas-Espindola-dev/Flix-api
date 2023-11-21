162b91fc8cdbfc6e312244d98689bd90a589cba2
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-21 10:28:57 -0300

    Creating Review admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from reviews.models import Review
 
-# Register your models here.
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('id', 'movie', 'stars')
+
+
+admin.site.register(Review, ReviewAdmin)
