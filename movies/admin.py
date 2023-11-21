d2707aeb37612bf73c32c580454a06d5283c2414
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-21 10:30:18 -0300

    Creating Movie admin

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from movies.models import Movie
 
-# Register your models here.
+
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+admin.site.register(Movie, MovieAdmin)
