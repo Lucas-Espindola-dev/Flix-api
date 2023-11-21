84699241717607b87b65892facbf9362c7c620a6
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-21 11:10:46 -0300

    Versioning genres urls

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('genres.urls')),
+    path('api/v1/', include('genres.urls')),
     path('', include('actors.urls')),
     path('', include('movies.urls')),
     path('', include('reviews.urls')),
