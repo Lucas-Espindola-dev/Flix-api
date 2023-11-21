774b5f6195adc30494f4b5311c57a72b552bccc8
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-21 14:39:41 -0300

    Validating realise date

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,25 +1,12 @@
 from rest_framework import serializers
 from movies.models import Movie
-# from genres.models import Genre
-# from actors.models import Actor
-
-
-# This is an example of how to do the Model Serializer on the hard way
-
-# class MovieSerializer(serializers.Serializer):
-#     title = serializers.CharField()
-#     genre = serializers.PrimaryKeyRelatedField(
-#         queryset=Genre.objects.all()
-#     )
-#     release_date = serializers.DateField()
-#     actors = serializers.PrimaryKeyRelatedField(
-#         queryset=Actor.objects.all(),
-#         many=True,
-#     )
-#     resume = serializers.CharField()
 
 
 class MovieModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = '__all__'
+
+    def validate_realise_date(self, value):
+        if value.year < 1970:
+            raise serializers.ValidationError('The realise date can not be before 1970.')
