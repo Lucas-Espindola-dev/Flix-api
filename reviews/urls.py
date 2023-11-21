eb171b4b4cf360a91dc1c37f950fa9e0e1a1f809
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-21 10:26:00 -0300

    Correcting Urlspatterns

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from reviews.views import ReviewCreateListView, ReviewRetrieveUpdateDestroyView
 
 
-urlpatters = [
+urlpatterns = [
     path('reviews/', ReviewCreateListView.as_view(), name='reviews-create-list'),
     path('reviews/<int:pk>/', ReviewRetrieveUpdateDestroyView.as_view(), name='reviews-detail-view'),
 ]
