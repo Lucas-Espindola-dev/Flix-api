af8952b4f345891276188d572383e899d68f3888
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-21 09:57:16 -0300

    Validators for the stars field

--- reviews/models.py
+++ reviews/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 from movies.models import Movie
 
 
 class Review(models.Model):
-    movie = models.ForeignKey(Movie, on_delete=)
+    movie = models.ForeignKey(Movie, on_delete=models.PROTECT, related_name='reviews')
+    stars = models.IntegerField(
+        validators=[
+            MinValueValidator(0, 'The valuation must not be inferior to 0'),
+            MaxValueValidator(5, 'The valuation must not be superior to 5'),
+        ])
+    comment = models.TextField(blank=True, null=True)
