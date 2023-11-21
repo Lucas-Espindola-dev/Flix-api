e94ee9519b21dfe9022e7e8b00313e69f36fd2e4
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-21 14:11:18 -0300

    Correcting the serializers name

--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,13 @@
 from rest_framework import generics
 from movies.models import Movie
-from movies.serializers import MovieSerializers
+from movies.serializers import MovieModelSerializer
 
 
 class MoviesCreateListView(generics.ListCreateAPIView):
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializers
+    serializer_class = MovieModelSerializer
 
 
 class MoviesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializers
+    serializer_class = MovieModelSerializer
