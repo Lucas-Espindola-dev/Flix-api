719c29c0b52cd27f1d5f77587badba0b668bbaa2
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-21 14:14:37 -0300

    Creating Movieserializers fields

--- movies/serializers.py
+++ movies/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from movies.models import Movie
 from genres.models import Genre
+from actors.models import Actor
 
 
 class MovieSerializer(serializers.Serializer):
     title = serializers.CharField
     genre = serializers.PrimaryKeyRelatedField(
         queryset=Genre.objects.all()
+    )
+    release_date = serializers.DateField()
+    actors = serializers.PrimaryKeyRelatedField(
+        queryset=Actor.objects.all()
     )
 
 
