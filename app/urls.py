df0c741bbf4e23ba55f3a41022be7642116ec0fb
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-11-21 14:10:11 -0300

    Refactoring

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('api/v1/', include('genres.urls')),
     path('api/v1/', include('actors.urls')),
     path('api/v1/', include('movies.urls')),
