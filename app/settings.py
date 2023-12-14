6323b7fcf304c864816fbd21fb45f3619c945914
Lucas Espindola <lucasespindola.dev@gmail.com>
2023-12-14 15:24:14 -0300

    Adding new installed apps

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
 
+    'authentication',
     'genres',
     'actors',
     'movies',
