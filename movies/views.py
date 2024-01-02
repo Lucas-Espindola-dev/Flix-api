387fb3436a1703ed589ab038044a4274559376a9
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-02 15:17:49 -0300

    Creating permissions classes

--- movies/views.py
+++ movies/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
 from movies.models import Movie
 from movies.serializers import MovieModelSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class MoviesCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
 
 
 class MoviesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
