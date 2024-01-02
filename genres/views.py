f143fafd9630f1d3e0c99a9f20c22763cc42087b
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-02 15:17:55 -0300

    Creating permissions classes

--- genres/views.py
+++ genres/views.py
@@ -1,13 +1,16 @@
 from genres.models import Genre
 from rest_framework import generics
 from genres.serializers import GenreSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
