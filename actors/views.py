a67fa3f19706d3f6cd7b49c17df6c88d4b9c2077
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-02 15:11:13 -0300

    Creating permissions classes

--- actors/views.py
+++ actors/views.py
@@ -1,9 +1,11 @@
 from actors.models import Actor
+from rest_framework.permissions import IsAuthenticated
 from rest_framework import generics
 from actors.serializers import ActorSerializer
 
 
 class ActorCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
 
