dfac374b7e2046886eac8d40455c3fb5ad2bece7
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-02 14:43:02 -0300

    Including authentications urls

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('api/v1/', include('actors.urls')),
     path('api/v1/', include('movies.urls')),
     path('api/v1/', include('reviews.urls')),
+    path('api/v1/', include('authentication.urls')),
 ]
