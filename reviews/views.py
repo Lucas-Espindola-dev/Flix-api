d8a646b90772e63f30478a67ee94c5696426c885
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-02 15:18:01 -0300

    Creating permissions classes

--- reviews/views.py
+++ reviews/views.py
@@ -1,13 +1,16 @@
 from rest_framework import generics
 from reviews.models import Review
 from reviews.serializers import ReviewSerializer
+from rest_framework.permissions import IsAuthenticated
 
 
 class ReviewCreateListView(generics.ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
 
 
 class ReviewRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthenticated,)
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
