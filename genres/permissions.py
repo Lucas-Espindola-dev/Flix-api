edf22714fc6d5795f11cf911d5ae85f682314600
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-04 15:28:40 -0300

    Creating a has permission function

--- genres/permissions.py
+++ genres/permissions.py
@@ -4,4 +4,6 @@ from rest_framework import permissions
 class GenrePermissionClass(permissions.BasePermission):
 
     def has_permission(self, request, view):
+        if request.method == 'GET':
+            return request.user.has_permission('genres.view_genre')
         return False
