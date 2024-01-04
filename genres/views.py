f6149de1c1593446b94fc3e0ab1dd1717a65391a
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-04 15:05:45 -0300

    Adding GenrePermissionsClass to permission classes

--- genres/views.py
+++ genres/views.py
@@ -2,15 +2,16 @@ from genres.models import Genre
 from rest_framework import generics
 from genres.serializers import GenreSerializer
 from rest_framework.permissions import IsAuthenticated
+from genres.permissions import GenrePermissionClass
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GenrePermissionClass)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GenrePermissionClass)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
