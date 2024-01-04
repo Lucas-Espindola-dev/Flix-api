d0b5c6c3b29733cb45cbf55c3e88cd59c035471d
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-04 15:34:03 -0300

    Correcting has perm

--- genres/permissions.py
+++ genres/permissions.py
@@ -5,5 +5,5 @@ class GenrePermissionClass(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if request.method == 'GET':
-            return request.user.has_permission('genres.view_genre')
+            return request.user.has_perm('genres.view_genre')
         return False
