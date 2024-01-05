67677f723deec9faf00fd6ada74d4d0967685209
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-05 15:00:18 -0300

    structuring function

--- app/permissions.py
+++ app/permissions.py
@@ -4,9 +4,15 @@ from rest_framework import permissions
 class GlobalDefaultPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        model_permission_codename = ''
+        model_permission_codename = self.__get_model_permission_codename(
+            method=request.method,
+            view=view
+        )
 
         return False
 
-    def get_model_permission_codename(self,method, view):
-        ...
+    def __get_model_permission_codename(self, method, view):
+        model_name = view.queryset.model._meta.model_name
+        app_label = view.queryset.model._meta.app_label
+        action = ''
+        return ''
