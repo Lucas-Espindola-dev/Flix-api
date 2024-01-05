132cb0cdb7bef12087607bcb274237b9ffdbe7fa
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-05 09:30:10 -0300

    Creating new permissions

--- genres/permissions.py
+++ genres/permissions.py
@@ -4,6 +4,9 @@ from rest_framework import permissions
 class GenrePermissionClass(permissions.BasePermission):
 
     def has_permission(self, request, view):
-        if request.method == 'GET':
+        if request.method in ['GET', 'OPTIONS', 'HEAD']:
             return request.user.has_perm('genres.view_genre')
+
+        if request.method == 'POST':
+            return request.user.has_perm('genres.add_genre')
         return False
