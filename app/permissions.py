8788039b9e6ddd199403c456e449201e8c4e0ba3
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-05 15:08:00 -0300

    structuring function

--- app/permissions.py
+++ app/permissions.py
@@ -8,14 +8,13 @@ class GlobalDefaultPermission(permissions.BasePermission):
             method=request.method,
             view=view
         )
-
-        return False
+        return request.user.has_perm(model_permission_codename)
 
     def __get_model_permission_codename(self, method, view):
         model_name = view.queryset.model._meta.model_name
         app_label = view.queryset.model._meta.app_label
         action = self.__get_action_sufix(method)
-        return ''
+        return f'{app_label}.{action}_{model_name}'
 
     def __get_action_sufix(self, method):
         method_actions = {
@@ -27,4 +26,4 @@ class GlobalDefaultPermission(permissions.BasePermission):
             'HEAD': 'view',
             'OPTIONS': 'view',
         }
-        return ''
+        return method_actions.get(method, '')
