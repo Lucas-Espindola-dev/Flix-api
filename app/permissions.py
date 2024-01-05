2edd4d4745d31affbeeef99dfa02d2aecd3d86d7
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-05 14:41:25 -0300

    Creating functions

--- app/permissions.py
+++ app/permissions.py
@@ -2,4 +2,11 @@ from rest_framework import permissions
 
 
 class GlobalDefaultPermission(permissions.BasePermission):
-    ...
+
+    def has_permission(self, request, view):
+        model_permission_codename = ''
+
+        return False
+
+    def get_model_permission_codename(self,method, view):
+        ...
