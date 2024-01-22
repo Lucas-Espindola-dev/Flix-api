ae0cc2e4283c8c669beaf41154eedfb776686c96
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-22 08:51:13 -0300

    Using global permissions

--- movies/views.py
+++ movies/views.py
@@ -2,15 +2,16 @@ from rest_framework import generics
 from movies.models import Movie
 from movies.serializers import MovieModelSerializer
 from rest_framework.permissions import IsAuthenticated
+from app.permissions import GlobalDefaultPermission
 
 
 class MoviesCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
 
 
 class MoviesRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer
