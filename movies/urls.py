40e504ebe5fde65fa8ddf0f6e5936c86b30fb878
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-22 10:55:27 -0300

    Creating path to url

--- movies/urls.py
+++ movies/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from movies.views import MoviesCreateListView, MoviesRetrieveUpdateDestroyView
+from movies.views import MoviesCreateListView, MoviesRetrieveUpdateDestroyView, MovieStatsAPI
 
 
 urlpatterns = [
     path('movies/', MoviesCreateListView.as_view(), name='movies-create-list'),
     path('movies/<int:pk>/', MoviesRetrieveUpdateDestroyView.as_view(), name='movies-detail-view'),
+    path('movies/stats/', MovieStatsAPI.as_view(), name='movies-stats')
 ]
