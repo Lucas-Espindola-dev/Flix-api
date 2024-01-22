18acaa097074d0221b40f51780170adde07aaa34
Lucas Espindola <lucasespindola.dev@gmail.com>
2024-01-22 08:51:05 -0300

    Using global permissions

--- genres/views.py
+++ genres/views.py
@@ -2,16 +2,16 @@ from genres.models import Genre
 from rest_framework import generics
 from genres.serializers import GenreSerializer
 from rest_framework.permissions import IsAuthenticated
-from genres.permissions import GenrePermissionClass
+from app.permissions import GlobalDefaultPermission
 
 
 class GenreCreateListView(generics.ListCreateAPIView):
-    permission_classes = (IsAuthenticated, GenrePermissionClass)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
 
 
 class GenreRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = (IsAuthenticated, GenrePermissionClass)
+    permission_classes = (IsAuthenticated, GlobalDefaultPermission,)
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
